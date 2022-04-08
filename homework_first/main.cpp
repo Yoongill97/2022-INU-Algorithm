9c1e01774ab26d468b562d5741f9df731f41cb2d
Yoongill97 <leem5449@naver.com>
2022-04-08 17:09:07 +0900

    homework_first

---
+++ homework_first/main.cpp
@@ -0,0 +1,19 @@
+//동적계획법 - 연속 행렬 곱셈
+
+
+#include <iostream>
+//각 조원들 헤더파일 포함
+
+using namespace std;
+
+int main(){
+    //행렬 입력
+    //실행 전 행렬 출력
+
+    //동적계획법 실행
+    //실행 후 행렬 출력
+
+    
+
+    return 0;
+};
