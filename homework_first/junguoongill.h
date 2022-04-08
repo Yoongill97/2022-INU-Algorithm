ecce54959be34db19f9dcc6502a77a016e057c8c
Yoongill97 <leem5449@naver.com>
2022-04-08 17:21:30 +0900

    add a jungyoongill.h file

---
+++ homework_first/junguoongill.h
@@ -0,0 +1,46 @@
+#ifndef __JUNGYOONGILL__LIB_H__
+#define __JUNGYOONGILL__LIB_H__
+
+#include <iostream>
+using namespace std;
+ 
+int ChainedMatrix(int arr[], int n)
+{
+    int** DP;
+    int i, j, k, L;
+    int tmp;
+ 
+    DP = new int*[n];
+    for (i = 0; i < n; i++)
+    {
+        DP[i] = new int[n];
+    }
+ 
+    for (i = 1; i < n; i++)
+    {
+        DP[i][i] = 0;
+    }
+ 
+    for (i = 1; i < n; i++)
+    {
+        for (j = 1; j < n - i; j++)
+        {
+            k = i + j;
+            DP[j][k] = INT32_MAX;
+            for (L = j; L <= k - 1; L++)
+            {
+                tmp = DP[j][L] + DP[L + 1][k] + arr[j - 1] * arr[L] * arr[k];
+                if (tmp < DP[j][k])
+                {
+                    DP[j][k] = tmp;
+                    //cout << tmp << endl;
+                }
+                cout << tmp << endl;
+            }
+        }
+    }
+ 
+    return DP[1][n - 1];
+}
+ 
+#endif
--- homework_first/main.cpp
+++ homework_first/main.cpp
@@ -2,15 +2,21 @@
 
 
 #include <iostream>
+#include "junguoongill.h"
+
+#define SIZE_OF_ARRAY 100
+
 //각 조원들 헤더파일 포함
 
 using namespace std;
 
 int main(){
-    //행렬 입력
+    //행렬 생성
+    int arr[SIZE_OF_ARRAY] = {0, };
     //실행 전 행렬 출력
 
     //동적계획법 실행
+    ChainedMatrix(arr, SIZE_OF_ARRAY);
     //실행 후 행렬 출력
 
     
